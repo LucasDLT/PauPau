410c0d4bd0ac5500b73326fa2d652d0ab6473d75
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-03-13 09:44:04 -0300

    feat:integracion de estado de apertua y cierre de menu
    feat:integracion de font julius sans para menu mobile
    
    feat:favicon integrado

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Julius_Sans_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const JuliusSansOne = Julius_Sans_One({
+weight:"400",
+variable:"--Julius-Sans-One"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${JuliusSansOne.variable} antialiased`}
       >
         {children}
       </body>
--- client/app/pages/init/index.tsx
+++ client/app/pages/init/index.tsx
@@ -1,13 +1,27 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export const Init = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false); //estado para abrir y cerrar el menu, agregar efectos de animacion al titulo.
+  const handleOpenMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <main className="bg-[url('/backgroundmobile.png')] bg-no-repeat bg-cover h-dvh grid grid-cols-[30px_1fr_30px]">
       <div className="bg-[url('/margins.png')] bg-no-repeat bg-cover "></div>
 
-      <div className="flex flex-col justify-center items-center">
-        <nav className="text-black hidden">
-          <ul>
+      <div className={`flex flex-col items-center
+     ${isOpen ? 'justify-around':'justify-center'}`}>
+        <Image
+          width={250}
+          height={50}
+          alt="Tilulo PauPau, arte en arcilla"
+          src={"/Pau Pau arte en arcilla Title.png"}
+          className="hover:cursor-pointer mt-1"
+        />
+        <nav className={`text-black ${isOpen ? "flex h-full" : "hidden"}`}>
+          <ul className="flex flex-col items-center justify-evenly Julius-Sans-One text-3xl">
             <li>
               <a href="#home">INICIO</a>
             </li>
@@ -22,23 +36,29 @@ export const Init = () => {
             </li>
           </ul>
         </nav>
-        <Image
-          width={250}
-          height={50}
-          alt="Tilulo PauPau, arte en arcilla"
-          src={"/Pau Pau arte en arcilla Title.png"}
-          className="hover:cursor-pointer"
-        />
       </div>
 
-      <div className="bg-[url('/margins.png')] bg-no-repeat bg-cover flex flex-col justify-evenly ">
-        <Image
-          width={30}
-          height={30}
-          src={"/menu.png"}
-          alt="icono de menu desplegable"
-          className="hover:cursor-pointer"
-        />
+      <div
+        className="bg-[url('/margins.png')] bg-no-repeat bg-cover flex flex-col justify-evenly "
+        onClick={handleOpenMenu}
+      >
+        {isOpen ? (
+          <Image
+            width={30}
+            height={30}
+            src={"/close.png"}
+            alt="icono cerrar menu desplegable"
+            className="hover:cursor-pointer"
+          />
+        ) : (
+          <Image
+            width={30}
+            height={30}
+            src={"/menuH.png"}
+            alt="icono de menu desplegable"
+            className="hover:cursor-pointer"
+          />
+        )}
         <Image
           width={30}
           height={30}
