9ac04703af62ef9ebd80af51a9b7a7f50d67679e
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-03-22 11:21:01 -0300

    feat:viewport desktop finalizado
    feat:animacion de entrada en items para ambos viewports integrada

---
+++ client/app/components/sectionText/index.tsx
@@ -0,0 +1,7 @@
+export const SectionText = () => {
+  return (
+    <div className="h-dvh text-3xl text-center Julius-Sans-One flex justify-center items-center border-b-2 border-t-2 ">
+      <p className="">. . . y estas son mis creaciones</p>
+    </div>
+  );
+};
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,14 @@
 import { Init } from "./pages/init";
 import { ProjectSection } from "./pages/project_section";
 import { Articles } from "./pages/articles";
+import { SectionText } from "./components/sectionText";
 export default function Home() {
   
   return (
     <>
       <Init />
       <ProjectSection />
-      <div className="h-100 text-3xl text-center Julius-Sans-One flex justify-center items-center border-b-2 border-t-2 ">
-        <p className="">. . . y estas son mis creaciones</p>
-      </div>
+      <SectionText/>
       <Articles />
  
     </>
