d82b7df942be05f8c0a67f0ff69510ea3d61e67c
Sebas de la Torre <lucasebasdlt@gmail.com>
2026-04-02 10:30:58 -0300

    feat-cart: UI finalizada para mobile y desktop

--- client/app/cart/page.tsx
+++ client/app/cart/page.tsx
@@ -9,7 +9,7 @@ export default function CartPage() {
   };
   return (
     //contenedor general del carrito
-    <section className="h-dvh Julius-Sans-One grid grid-cols-1 grid-rows-[20px_1fr_70px_auto] pb-10 gap-10 md:grid-rows-[20px_1fr_70px_450px]">
+    <section className="h-dvh Julius-Sans-One grid grid-cols-1 grid-rows-[20px_1fr_70px_auto] pb-10 gap-10 md:grid-rows-[20px_1fr_50px_70px_450px]">
       <button onClick={handleGoBack}>
         <Image
           src={"/circulo-flecha.png"}
@@ -79,12 +79,12 @@ export default function CartPage() {
         </button>
         <p className="border p-1 rounded ">TOTAL:10000</p>
       </div>
-
+      <p className="self-center text-center py-6 text-3xl  border-yellow-600 border-dashed border-y-2 font-black ">completa el formulario y envialo para coordinar pago y envio.</p>
       <form
         action="submit"
-        className="font-black grid grid-cols-1 grid-rows-3 gap-8 pb-10 md:grid-rows-2 md:grid-cols-[800px_400px]"
+        className="font-black grid grid-cols-1 grid-rows-[1fr_80px_60px] gap-8 pb-10 md:grid-rows-2 md:grid-cols-[1fr_600px]"
       >
-        <div className="md:col-start-1 md:grid md:gap-6 ">
+        <div className="flex flex-col gap-5 md:col-start-1 md:grid md:gap-8 ">
           <div className="border-b flex gap-2">
             <label htmlFor="">Nombre</label>
             <input type="text" className="custom-input font-light" />
@@ -127,18 +127,22 @@ export default function CartPage() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-1 text-[14px] md:col-start-2 ">
-          <div className="checkbox-item border rounded-full flex justify-between items-center h-6 px-2">
+        <div className="grid gap-1 text-[14px] items-center justify-center md:col-start-2 md:row-start-1 md:self-end ">
+          <div className="checkbox-item border rounded-full flex justify-between items-center h-8 px-2">
             <label htmlFor="encuentro">punto de encuentro sin costo</label>
             <input type="checkbox" id="encuentro" className="custom-checkbox" />
           </div>
 
-          <div className="checkbox-item border rounded-full flex justify-between items-center h-6 px-2">
+          <div className="checkbox-item border rounded-full flex justify-between items-center h-8 px-2">
             <label htmlFor="envio">envio con costo a coordinar</label>
             <input type="checkbox" id="envio" className="custom-checkbox" />
           </div>
         </div>
-        <button className="Alan-Sans border rounded md:col-start-2 md:row-start-2 md:h-8 md:hover:cursor-pointer">ENVIAR PEDIDO</button>
+        <div className="Alan-Sans justify-self-center self-center  md:col-start-2 md:row-start-2  md:justify-self-center ">
+          <button className="bg-olive-500/30 p-1 border rounded  md:h-8 md:hover:cursor-pointer ">
+            ENVIAR PEDIDO
+          </button>
+        </div>
       </form>
     </section>
   );
